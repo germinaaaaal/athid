58c1f814189e1b7d1db5da3911aaf7e42d25785a
Casimir Gaul-Berrard <germinaaaaal@gmail.com>
2019-02-05 07:43:59 +0100

    create orthography.py

---
+++ orthography.py
@@ -0,0 +1,15 @@
+#orthography.py
+#an ascii-to-áthid orthography tool that runs in the terminal
+
+while True:
+    text = input("text to romanize: ")
+    if text == "quit":
+        break
+    else:
+        for char in text:
+            text = text.replace("z", "ʒ")
+            text = text.replace("x", "ś")
+            text = text.replace("th", "ý")
+            text = text.replace("q", "ź")
+            text = text.replace("v", "ẃ")
+        print(text)
